85974c44d2c87568384bc17cdb19c1953055bd77
Gabriele <gabri.cabal@gmail.com>
2021-04-15 15:43:03 +0200

    Updated widget tree formatting and organizing

--- lib/safe-zone-home.dart
+++ lib/safe-zone-home.dart
@@ -25,7 +25,11 @@ class SafeZoneHome extends StatefulWidget {
 class _SafeZoneHome extends State<SafeZoneHome> {
   // Logged user informations
   final MyOlderUser _user;
+
+  // Text edit controller for the input search box
   var _searchController = TextEditingController();
+
+  // Safe file manager
   final SafeFileManager _manager;
 
   _SafeZoneHome(this._user, this._manager);
@@ -43,198 +47,189 @@ class _SafeZoneHome extends State<SafeZoneHome> {
     });
   }
 
-  /// Carica la lista dei file da mostrare e gestire nella safe zone
-  Future<void> loadSafeFilesList() {
-    // TODO: Implement loading files from container file
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title:
-              Text('Safe zone', style: Theme.of(context).textTheme.headline1),
-          actions: [
-            IconButton(
-              icon: Icon(
-                Icons.logout,
-                size: Theme.of(context).iconTheme.size,
-                color: Theme.of(context).iconTheme.color,
-              ),
-              onPressed: () {
-                doLogout(context);
-              },
-            )
-          ],
-        ),
-        body: Column(
-          children: [
-            Container(
-                padding:
-                    EdgeInsets.only(left: 20, right: 20, top: 10, bottom: 10),
-                height: 65,
-                child: TextField(
-                    controller: _searchController,
-                    maxLines: 1,
-                    decoration: InputDecoration(
-                        prefixIcon: Icon(
-                          Icons.text_format,
-                          size: Theme.of(context).iconTheme.size,
-                        ),
-                        suffix: IconButton(
-                            icon: Icon(
-                              Icons.search,
-                              size: Theme.of(context).iconTheme.size,
-                              color: Theme.of(context).iconTheme.color,
-                            ),
-                            onPressed: () {
-                              // Start research for files with this name
-                              startSearch(_searchController.text);
-                            }),
-                        border: OutlineInputBorder(
-                            borderRadius: BorderRadius.all(Radius.circular(10)),
-                            borderSide:
-                                BorderSide(color: Colors.black, width: 1))))),
-            Expanded(
-                child: ListView.builder(
-                    itemCount: _manager.safeFilesCount,
-                    itemBuilder: (context, index) {
-                      return SafeFileWidget(
-                          _manager.safeFiles[index], (String tag) {});
-                    }))
-          ],
-        ),
-        backgroundColor: Theme.of(context).backgroundColor,
-        drawer: Drawer(
-            child: ListView(
-          children: [
-            Padding(
-                padding: EdgeInsets.only(top: 40),
-                child: Center(
-                    child: Text('MyOlder Safe Zone',
-                        style: Theme.of(context).textTheme.bodyText1))),
-            Container(
-              margin: EdgeInsets.all(20),
-              child: MaterialButton(
-                onPressed: () {
-                  showUserSettings();
-                },
-                color: Theme.of(context).backgroundColor,
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.all(Radius.circular(20)),
-                    side: BorderSide(width: 1.5, color: Colors.black)),
-                child: Row(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Container(
-                        margin: EdgeInsets.all(20) - EdgeInsets.only(left: 15),
-                        child: Icon(Icons.person,
-                            size: 70, color: Colors.tealAccent),
-                        decoration: BoxDecoration(
-                            color: Colors.white,
-                            border: Border.all(color: Colors.black, width: 1.5),
-                            borderRadius:
-                                BorderRadius.all(Radius.circular(35)))),
-                    Column(
-                      children: [
-                        Padding(
-                            padding: EdgeInsets.only(top: 40),
-                            child: Text(
-                              '${_user.name}',
-                              style: Theme.of(context).textTheme.headline4,
-                              overflow: TextOverflow.ellipsis,
-                            )),
-                        Padding(
-                            padding: EdgeInsets.only(top: 10),
-                            child: Text('Authenticated user'))
-                      ],
-                    )
-                  ],
+      appBar: AppBar(
+        title: Text('Safe zone', style: Theme.of(context).textTheme.headline1),
+        actions: [
+          IconButton(
+            icon: Icon(
+              Icons.logout,
+              size: Theme.of(context).iconTheme.size,
+              color: Theme.of(context).iconTheme.color,
+            ),
+            onPressed: () {
+              doLogout(context);
+            },
+          ),
+        ],
+      ),
+      body: Column(
+        children: [
+          Container(
+            padding: EdgeInsets.only(left: 20, right: 20, top: 10, bottom: 10),
+            height: 65,
+            child: TextField(
+              controller: _searchController,
+              maxLines: 1,
+              decoration: InputDecoration(
+                prefixIcon: Icon(
+                  Icons.text_format,
+                  size: Theme.of(context).iconTheme.size,
+                ),
+                suffix: IconButton(
+                    icon: Icon(
+                      Icons.search,
+                      size: Theme.of(context).iconTheme.size,
+                      color: Theme.of(context).iconTheme.color,
+                    ),
+                    onPressed: () {
+                      // Start research for files with this name
+                      startSearch(_searchController.text);
+                    }),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(10)),
+                  borderSide: BorderSide(color: Colors.black, width: 1),
                 ),
               ),
             ),
-            DrawerLongButton(
-              text: 'Application informations',
-              icon: Icon(
-                Icons.info,
-                size: 20,
-                color: Colors.black,
-              ),
-              callBack: () {
-                showApplicationInformations();
-              },
+          ),
+          Expanded(
+            child: ListView.builder(
+                itemCount: _manager.safeFilesCount,
+                itemBuilder: (context, index) {
+                  return SafeFileWidget(
+                      _manager.safeFiles[index], (String tag) {});
+                }),
+          ),
+        ],
+      ),
+      backgroundColor: Theme.of(context).backgroundColor,
+      drawer: Drawer(
+          child: ListView(
+        children: [
+          Padding(
+            padding: EdgeInsets.only(top: 40),
+            child: Center(
+              child: Text('MyOlder Safe Zone',
+                  style: Theme.of(context).textTheme.bodyText1),
             ),
-            DrawerLongButton(
-              text: 'Settings',
-              icon: Icon(
-                Icons.settings,
-                size: 20,
-                color: Colors.black,
-              ),
-              callBack: () {
-                showApplicationSettings();
+          ),
+          Container(
+            margin: EdgeInsets.all(20),
+            child: MaterialButton(
+              onPressed: () {
+                showUserSettings();
               },
-            ),
-            DrawerLongButton(
-              text: 'Add new safe file',
-              icon: Icon(
-                Icons.add,
-                size: 20,
-                color: Colors.black,
+              color: Theme.of(context).backgroundColor,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.all(Radius.circular(20)),
+                side: BorderSide(width: 1.5, color: Colors.black),
               ),
-              callBack: (){
-                addNewFile(context);
-                Navigator.pop(context);
-              },
-            ),
-            DrawerLongButton(
-              text: 'Clear safe zone',
-              icon: Icon(
-                Icons.delete,
-                size: 20,
-                color: Colors.black
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Container(
+                    margin: EdgeInsets.all(20) - EdgeInsets.only(left: 15),
+                    child:
+                        Icon(Icons.person, size: 70, color: Colors.tealAccent),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      border: Border.all(color: Colors.black, width: 1.5),
+                      borderRadius: BorderRadius.all(Radius.circular(35)),
+                    ),
+                  ),
+                  Column(
+                    children: [
+                      Padding(
+                        padding: EdgeInsets.only(top: 40),
+                        child: Text(
+                          '${_user.name}',
+                          style: Theme.of(context).textTheme.headline4,
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.only(top: 10),
+                        child: Text('Authenticated user'),
+                      ),
+                    ],
+                  )
+                ],
               ),
-              callBack: (){
-                clearSafeZone();
-              },
             ),
-            DrawerLongButton(
-              text: 'MyOlder FAQ',
-              icon: Icon(
-                Icons.question_answer,
-                size: 20,
-                color: Colors.black
-              ),
-              callBack: (){
-                showApplicationFAQ();
-              }
+          ),
+          DrawerLongButton(
+            text: 'Application informations',
+            icon: Icon(
+              Icons.info,
+              size: 20,
+              color: Colors.black,
             ),
-            DrawerLongButton(
-              text: 'Logout',
-              icon: Icon(
-                Icons.logout,
-                color: Colors.black,
-                size: 20
-              ),
-              callBack: (){
-                doLogout(context);
-              },
-            )
-          ],
-        )),
-        floatingActionButton: FloatingActionButton(
-            onPressed: () {
-              addNewFile(context);
+            callBack: () {
+              showApplicationInformations();
             },
-            backgroundColor:
-                Theme.of(context).floatingActionButtonTheme.backgroundColor,
-            child: Icon(
-              Icons.add,
-              size: Theme.of(context).iconTheme.size + 10,
-              color: Theme.of(context).iconTheme.color,
+          ),
+          DrawerLongButton(
+            text: 'Settings',
+            icon: Icon(
+              Icons.settings,
+              size: 20,
+              color: Colors.black,
             ),
-            splashColor:
-                Theme.of(context).floatingActionButtonTheme.splashColor));
+            callBack: () {
+              showApplicationSettings();
+            },
+          ),
+          DrawerLongButton(
+            text: 'Add new safe file',
+            icon: Icon(
+              Icons.add,
+              size: 20,
+              color: Colors.black,
+            ),
+            callBack: () {
+              addNewFile(context);
+              Navigator.pop(context);
+            },
+          ),
+          DrawerLongButton(
+            text: 'Clear safe zone',
+            icon: Icon(Icons.delete, size: 20, color: Colors.black),
+            callBack: () {
+              clearSafeZone();
+            },
+          ),
+          DrawerLongButton(
+            text: 'MyOlder FAQ',
+            icon: Icon(Icons.question_answer, size: 20, color: Colors.black),
+            callBack: () {
+              showApplicationFAQ();
+            },
+          ),
+          DrawerLongButton(
+            text: 'Logout',
+            icon: Icon(Icons.logout, color: Colors.black, size: 20),
+            callBack: () {
+              doLogout(context);
+            },
+          ),
+        ],
+      )),
+      floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            addNewFile(context);
+          },
+          backgroundColor:
+              Theme.of(context).floatingActionButtonTheme.backgroundColor,
+          child: Icon(
+            Icons.add,
+            size: Theme.of(context).iconTheme.size + 10,
+            color: Theme.of(context).iconTheme.color,
+          ),
+          splashColor: Theme.of(context).floatingActionButtonTheme.splashColor),
+    );
   }
 
   /// Adds a new file to the safe zone
@@ -263,30 +258,27 @@ class _SafeZoneHome extends State<SafeZoneHome> {
     }
   }
 
-  Future<void> clearSafeZone() async{
-    Navigator.push(context, MaterialPageRoute(builder: (context){
+  Future<void> clearSafeZone() async {
+    Navigator.push(context, MaterialPageRoute(builder: (context) {
       return AlertDialog(
-        title: Text(
-          'Alert: clear all files?'
-        ),
+        title: Text('Alert: clear all files?'),
         content: Text(
-          'This operation will remove all your data from the disk, this means that you wont be able to access your files anymore. ARE YOU SHURE??'),
+            'This operation will remove all your data from the disk, this means that you wont be able to access your files anymore. ARE YOU SHURE??'),
         actions: [
           TextButton(
             child: Text('No'),
-            onPressed: (){
+            onPressed: () {
               // Remove this alert
               Navigator.pop(context);
             },
           ),
           TextButton(
             child: Text('Yes i am shure. '),
-            onPressed: (){
+            onPressed: () {
               _manager.clearAllSafeFiles();
               Navigator.pop(context);
               Navigator.pop(context);
-              setState(() {
-              });
+              setState(() {});
             },
           )
         ],
@@ -294,11 +286,11 @@ class _SafeZoneHome extends State<SafeZoneHome> {
     }));
   }
 
-  Future<void> showApplicationFAQ() async{
+  Future<void> showApplicationFAQ() async {
     // TODO: Implement application faq
   }
 
-  Future<void> showUserSettings() async{
+  Future<void> showUserSettings() async {
     // TODO: Implement user settings
   }
 
@@ -306,7 +298,7 @@ class _SafeZoneHome extends State<SafeZoneHome> {
     // TODO: Implement application informations
   }
 
-  Future<void> showApplicationSettings() async{
+  Future<void> showApplicationSettings() async {
     // TODO: Implement application settings
   }
 
