483c2fc8499805b9373e953c868e07c35a423bba
Gabriele <gabri.cabal@gmail.com>
2021-05-06 20:14:50 +0200

    Made all fields public and updated doc

--- my_older/lib/exceptions/element-not-found-exception.dart
+++ my_older/lib/exceptions/element-not-found-exception.dart
@@ -1,20 +1,18 @@
 /// Created on 12/04/21 from gabriele
 /// Project myolder
 
-// TODO: Make all fields public and use named constructor for the exception
 class ElementNotFoundException implements Exception{
-    final String _elementName;
-    final String _elementType;
-    final String _container;
+    final String elementName;
+    final String elementType;
+    final String container;
 
     /// Creates a new instance of an element not found exception
     ///
-    /// [_elementName] Represents the element that wasn't found
-    /// [_elementType] Represents the type of the element that wasn't found
-    /// [_container] The place where the element should be saved
-    /// TODO: Update documentation
-    ElementNotFoundException(this._elementName, this._elementType, this._container);
+    /// [elementName] Represents the element that wasn't found
+    /// [elementType] Represents the type of the element that wasn't found
+    /// [container] The place where the element should be saved
+    ElementNotFoundException(this.elementName, this.elementType, this.container);
     
     @override
-    String toString() => 'ElementNotFoundException. The element $_elementName wasnt found in the list $_container. The type is: $_elementType';
+    String toString() => 'ElementNotFoundException. The elementName wasnt found in the list $container. The type is: $elementType';
 }
