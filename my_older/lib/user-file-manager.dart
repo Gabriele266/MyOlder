111bd479bf7042a2abcfb670785685a494677de1
Gabriele <gabri.cabal@gmail.com>
2021-05-06 20:38:55 +0200

    Refactoring and todos

--- my_older/lib/user-file-manager.dart
+++ my_older/lib/user-file-manager.dart
@@ -1,8 +1,8 @@
-import 'package:flutter/material.dart';
+import 'dart:io';
+
+import 'package:flutter/foundation.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:aes_crypt/aes_crypt.dart';
-
-import 'dart:io';
 
 import 'exceptions/null-data-exception.dart';
 import 'myolder-user.dart';
@@ -10,34 +10,33 @@ import 'exceptions/file-not-defined-exception.dart';
 import 'exceptions/file-not-found-exception.dart';
 import 'exceptions/user-not-defined-exception.dart';
 
-// TODO: Make all the fields public and remove getters/setters
 class UserFileManager {
-  String _file;
-  MyOlderUser _user;
+  String file;
+  MyOlderUser user;
 
-  /// Inizializza una lettore impostando il file da cui leggere e l'utente con cui confrontarlo
+  /// Inizializza una [UserFileManager] impostando il file da cui leggere e l'utente con cui confrontarlo
   ///
   /// [file] : Rappresenta il file da cui leggere
   /// [user] : Le informazioni dell' utente con cui controllare
-  UserFileManager({String file, MyOlderUser user}) {
-    _file = file;
-    _user = user;
-  }
+  UserFileManager({@required this.file, @required this.user});
 
   /// Starts the control of the user and checks if it is allowed to access the application
   ///
-  /// Needs **_file** to pick up the file from the file system and **_user** to check the informations
-  /// Returns true if this user is allowed to access the application, false otherwise. In case of errors an exception is
-  /// thrown
+  /// Needs [file] to pick up the file from the file system and [user] to check the informations
+  /// Returns 'true' if this user is allowed to access the application, 'false' otherwise.
+  /// In case of errors an exception is thrown.
+  ///
+  /// If the configuration file doesn't exists, a [FileNotFoundException] is thrown
+  /// If the [file] is empty, then a [NullDataException] is thrown
   /// TODO: Implement use of xml for the root file
   Future<bool> doControl() async {
-    if (_file.isNotEmpty) {
+    if (file.isNotEmpty) {
       // Get the file object
-      var file =
-          File('${(await getApplicationDocumentsDirectory()).path}/$_file');
+      var oFile =
+          File('${(await getApplicationDocumentsDirectory()).path}/$file');
 
       // Check if file exists
-      if (file.existsSync()) {
+      if (oFile.existsSync()) {
         // Leggo le righe del file
         final decr = AesCrypt(
           '%#^363#&3696^&^543&5335&73&55&2@6%232!^432!^6%&842427@!7%98^@824#969#836^@2889!862^@77#2!72355%',
@@ -45,10 +44,10 @@ class UserFileManager {
 
         // Get the file lines
         final fileLines =
-            (await decr.decryptTextFromFile(file.path)).split('\n');
+            (await decr.decryptTextFromFile(oFile.path)).split('\n');
 
         // Nome utente
-        var user = '';
+        var usr = '';
         // Password
         var password = '';
 
@@ -56,7 +55,7 @@ class UserFileManager {
           if (!line.startsWith('#')) {
             // Controllo se si tratta di una riga che definisce il nome utente per l'accesso
             if (line.startsWith('user-name')) {
-              user = line.split(':')[1];
+              usr = line.split(':')[1];
             } else if (line.startsWith('password')) {
               password = line.split(':')[1];
               break;
@@ -64,16 +63,16 @@ class UserFileManager {
           }
         }
 
-        if (_user.name == user && _user.password == password) {
-          return true;
-        }
-        return false;
-      } else {
-        throw FileNotFoundException(file: _file, path: file.path);
-      }
+        // return the result of the check
+        return (user.name == usr && user.password == password);
+      } else
+        throw FileNotFoundException(
+          file: file,
+          path: oFile.path,
+        );
     } else
       throw NullDataException(
-        data: '_file',
+        data: 'file',
         operationDescription:
             'Executing the first control step for the credentials.',
         function: 'doControl',
@@ -99,19 +98,21 @@ class UserFileManager {
 
   /// Checks if the user credentials are allowed to access the application or not
   ///
-  /// Needs that the internal member _file is set.
-  /// [_file] represents the the file to search
+  /// Needs that the internal member file is set.
+  /// [file] represents the the file to search
+  /// 
+  /// If the [file] is empty (no file specified) then a [NullDataException] is thrown
   Future<bool> checkRootExists() async {
-    if (_file.isNotEmpty) {
+    if (file.isNotEmpty) {
       // Get the file formatting his path
-      final file =
-          File('${(await getApplicationDocumentsDirectory()).path}/$_file');
+      final oFile =
+          File('${(await getApplicationDocumentsDirectory()).path}/$file');
 
       // Check if file exists
-      return file.existsSync();
+      return oFile.existsSync();
     } else
       throw NullDataException(
-        data: '_file',
+        data: 'file',
         operationDescription: 'checking if the root file exists',
         function: 'UserFileManager::checkRootExists',
       );
@@ -119,15 +120,15 @@ class UserFileManager {
 
   /// Starts writing the user informations on the file
   ///
-  /// Needs that the member _file and _user of this class are set.
+  /// Needs that the member file and user of this class are set.
   /// TODO: Implement password generation
   Future<void> writeFile() async {
     // Formatto il percorso del file da creare
-    if (_file.isNotEmpty) {
+    if (file.isNotEmpty) {
       // Check if the user informations are set
-      if (_user != null && _user.isNotEmpty()) {
+      if (user != null && user.isNotEmpty()) {
         final String filePath =
-            '${(await getApplicationDocumentsDirectory()).path}/$_file';
+            '${(await getApplicationDocumentsDirectory()).path}/$file';
 
         // Encrypt all the informations
         final crt = AesCrypt(
@@ -137,24 +138,22 @@ class UserFileManager {
         crt.encryptTextToFile(
           '# user informations. \n'
           'creation-time:${DateTime.now().toString()}\n'
-          'user-name:${_user.name}\n'
-          'password:${_user.password}\n',
+          'user-name:${user.name}\n'
+          'password:${user.password}\n',
           filePath,
         );
       } else
-        throw UserNotDefinedException(operation: 'writeFile() async');
+        throw UserNotDefinedException('writeFile() async');
     } else
-      throw FileNotDefinedException(operation: 'writeFile() async');
+      throw FileNotDefinedException('writeFile() async');
   }
 
   /// Removes the configuration file
   Future<void> removeFile() async {
     final String filePath =
-        '${(await getApplicationDocumentsDirectory()).path}/$_file';
-    final file = File(filePath);
+        '${(await getApplicationDocumentsDirectory()).path}/$file';
+    final oFile = File(filePath);
     // Check if file exists and delete it
-    if (file.existsSync()) file.deleteSync();
+    if (oFile.existsSync()) oFile.deleteSync();
   }
 }
-
-
