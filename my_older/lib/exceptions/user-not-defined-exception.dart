578247fdc0657f4b43b65758b3790e164aa4cf2e
Gabriele <gabri.cabal@gmail.com>
2021-05-06 20:39:13 +0200

    Migrated to positional argument for constructor

--- my_older/lib/exceptions/user-not-defined-exception.dart
+++ my_older/lib/exceptions/user-not-defined-exception.dart
@@ -1,16 +1,16 @@
 import 'package:flutter/foundation.dart';
 
 class UserNotDefinedException implements Exception {
-    final String operation;
+  final String operation;
 
-    /// Creates a new [UserNotDefinedException]
-    /// 
-    /// This exception can be thrown when a user isn't specified during an [operation]
-    /// e.g an authenticating operation. 
-    const UserNotDefinedException({@required this.operation});
+  /// Creates a new [UserNotDefinedException]
+  ///
+  /// This exception can be thrown when a user isn't specified during an [operation]
+  /// e.g an authenticating operation.
+  const UserNotDefinedException(this.operation);
 
-    @override
-    String toString() {
-        return 'UserNotDefinedException: During the operation $operation a null user was given to the function. ';
-    }
+  @override
+  String toString() {
+    return 'UserNotDefinedException: During the operation $operation a null user was given to the function. ';
+  }
 }
