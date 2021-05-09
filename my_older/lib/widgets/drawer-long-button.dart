5c46114aaacde27a8aae3680a04fd5cd407847c4
Gabriele <gabri.cabal@gmail.com>
2021-05-09 20:52:37 +0200

    Improved DrawerListTileButton

--- my_older/lib/widgets/drawer-long-button.dart
+++ my_older/lib/widgets/drawer-long-button.dart
@@ -2,46 +2,56 @@
 /// Project myolder
 import 'package:flutter/material.dart';
 
-class DrawerLongButton extends StatelessWidget {
+class DrawerListTileButton extends StatelessWidget {
   final String text;
   final void Function() callBack;
-  final Icon icon;
+  final IconData icon;
 
   /// Creates new instance of a DrawerLongButton
   ///
   /// It represents a long button with some properties
   /// [text] The text of the button
-  /// [icon] The icon of the button
+  /// [icon] The icon data of the button (for color and size is used the [accentIconTheme])
   /// [callBack] The callback of the button
-  DrawerLongButton({
+  DrawerListTileButton({
     Key key,
     @required this.text,
     this.callBack,
     this.icon,
   }) : super(key: key);
 
+  // TODO: Make this widget responsive
   @override
   Widget build(BuildContext context) {
-    return Flex(direction: Axis.horizontal, children: [
-      Expanded(
-        child: MaterialButton(
-          onPressed: callBack,
-          color: Theme.of(context).canvasColor,
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              if (icon != null)
-                Padding(
-                  child: icon,
-                  padding: const EdgeInsets.only(right: 20),
-                ),
-              Text(
-                text,
-              ),
-            ],
+    final theme = Theme.of(context);
+    final media = MediaQuery.of(context);
+
+    return Container(
+      height: media.size.height * 0.06,
+      margin: EdgeInsets.symmetric(
+        horizontal: media.size.width * 0.05,
+        vertical: media.size.height * 0.010,
+      ),
+      child: ListTile(
+        shape: RoundedRectangleBorder(
+          side: BorderSide(
+            color: theme.primaryColorDark,
+            width: 2 * media.textScaleFactor,
           ),
+          borderRadius: BorderRadius.circular(10),
+        ),
+        tileColor: theme.primaryColor,
+        onTap: callBack,
+        leading: Icon(
+          icon,
+          size: theme.accentIconTheme.size + (10 * media.textScaleFactor),
+          color: theme.accentIconTheme.color,
+        ),
+        title: Text(
+          '$text',
+          style: theme.textTheme.headline3,
         ),
       ),
-    ]);
+    );
   }
 }
