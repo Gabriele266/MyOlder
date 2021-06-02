cb3001003181549dc32dad406cee38919c7bc6a2
Gabriele <gabri.cabal@gmail.com>
2021-06-02 19:09:33 +0200

    Adjusted responsiveness and pattern

--- my_older/lib/pages/newuser-info-page.dart
+++ my_older/lib/pages/newuser-info-page.dart
@@ -1,6 +1,7 @@
 import 'dart:ui';
 
 import 'package:flutter/material.dart';
+import '../widgets/text-section.dart';
 
 /// TODO: Make NewUserInfoPage page responsive and adaptive
 class NewUserInfoPage extends StatelessWidget {
@@ -14,68 +15,54 @@ class NewUserInfoPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    // Simplify
+    final theme = Theme.of(context);
+    final media = MediaQuery.of(context);
+
     return Scaffold(
-      backgroundColor: Theme.of(context).backgroundColor,
+      backgroundColor: theme.backgroundColor,
       appBar: AppBar(
         title: Text(
           'New user informations',
-          style: Theme.of(context).textTheme.headline1,
+          style: theme.textTheme.headline1,
         ),
       ),
-      body: Center(
-        child: Container(
-          margin: const EdgeInsets.only(left: 10, right: 10),
-          child: Column(
-            children: [
-              Padding(
-                padding: const EdgeInsets.only(top: 30, left: 20),
-                child: Text(
-                  'New user creation informations',
-                  style: Theme.of(context).textTheme.headline1,
-                  textAlign: TextAlign.center,
+      body: SingleChildScrollView(
+        child: Center(
+          child: Container(
+            margin: EdgeInsets.symmetric(horizontal: media.size.width * 0.02),
+            child: Column(
+              children: [
+                TextSection(
+                  title: 'Creation informations',
+                  titleIcon: Icons.info,
+                  content:
+                      'To set up a safe-zone you need to create a user that can access the files. ',
+                  maxHeight: media.size.height * 0.2,
                 ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(top: 10),
-                child: Text(
-                  'To set up a safe-zone you need to create a user that can access the files. ',
-                  style: Theme.of(context).textTheme.headline2,
-                  textAlign: TextAlign.center,
+                TextSection(
+                  title: 'User informations',
+                  content:
+                      'In order to create a new user for this safe zone, you have to provide us some informations. You should give it a '
+                      'username and assign a password. These will be asked at every log-in. ',
+                  maxHeight: media.size.height * 0.25,
                 ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(top: 30),
-                child: Text(
-                  'User informations',
-                  style: Theme.of(context).textTheme.headline1,
+                TextSection(
+                  title: 'Rules',
+                  content:
+                      'Follow this simple rules: The username must only contain lower case characters or numbers, and the password should be longer than 6 characters. Thank you!!!',
+                  maxHeight: media.size.height * 0.2,
                 ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(top: 10),
-                child: Text(
-                  'In order to create a new user for this safe zone, you have to provide us some informations. You should give it a '
-                  'UserName and assign a password. This two will be asked at every log-in. ',
-                  style: Theme.of(context).textTheme.headline2,
-                  textAlign: TextAlign.center,
+                Center(
+                  child: TextButton(
+                    onPressed: () => Navigator.pop(context),
+                    child: Text(
+                      'Understood',
+                    ),
+                  ),
                 ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(top: 30),
-                child: Text(
-                  'Rules',
-                  style: Theme.of(context).textTheme.headline1,
-                  textAlign: TextAlign.center,
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(top: 10),
-                child: Text(
-                  'Follow this simple rules: The username must only contain lower case characters or numbers, and the password should be longer than 6 characters. Thank you!!!',
-                  style: Theme.of(context).textTheme.headline2,
-                  textAlign: TextAlign.center,
-                ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
