21acee52c8a04a35688dba49f842e31cbc9000fa
Gabriele <gabri.cabal@gmail.com>
2021-06-02 18:50:36 +0200

    Adjusted responsiveness and pattern

--- my_older/lib/pages/login-info-page.dart
+++ my_older/lib/pages/login-info-page.dart
@@ -1,75 +1,59 @@
 import 'package:flutter/material.dart';
 
-// TODO: Use better pattern for this page
-// TODO: Optimize Theme and Media with final variables
-// TODO: Make this page responsive and adaptive
+import '../widgets/text-section.dart';
+
 class LoginInfoPage extends StatelessWidget {
   /// The name of this page
   static const String routeName = '/login/info';
 
   @override
   Widget build(BuildContext context) {
+    // Simplify
+    final theme = Theme.of(context);
+    final media = MediaQuery.of(context);
+
     return Scaffold(
-      backgroundColor: Theme.of(context).backgroundColor,
+      backgroundColor: theme.backgroundColor,
       appBar: AppBar(
         title: Text(
           'Login informations',
-          style: Theme.of(context).textTheme.headline1,
+          style: theme.textTheme.headline1,
         ),
       ),
-      body: Center(
-        child: Container(
-          margin: const EdgeInsets.only(
-            left: 10,
-            right: 10,
-          ),
-          child: Column(
-            children: [
-              Padding(
-                  padding: const EdgeInsets.only(top: 30, left: 20),
-                  child: Text('Credentials needed',
-                      style: Theme.of(context).textTheme.headline1)),
-              Padding(
-                padding: const EdgeInsets.only(top: 10),
-                child: Text(
-                  'To access the safe zone you need to insert your credentials. The manager should give you a UserName and a Password to use this application. ',
-                  style: Theme.of(context).textTheme.headline2,
-                  textAlign: TextAlign.center,
+      body: SingleChildScrollView(
+        child: Center(
+          child: Container(
+            margin: EdgeInsets.symmetric(horizontal: media.size.width * 0.02),
+            child: Column(
+              children: [
+                TextSection(
+                  title: 'Credentials needed',
+                  content:
+                      'To access the safe zone you need to insert your credentials. Theme manager should give you a username and a password to use this application. ',
+                  maxHeight: media.size.height * 0.25,
                 ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(top: 30),
-                child: Text(
-                  'The only way to access data is using this',
-                  style: Theme.of(context).textTheme.headline1,
-                  textAlign: TextAlign.center,
+                TextSection(
+                  title: 'The only way to access data is using this',
+                  content:
+                      'If you have lost them, you won\'t be able to access all your data!!!! Pay attention!!!',
+                  maxHeight: media.size.height * 0.2,
                 ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(top: 10),
-                child: Text(
-                  'If you have lost them, you won\'t be able to access all your data!!!! Pay attention!!!',
-                  style: Theme.of(context).textTheme.headline2,
-                  textAlign: TextAlign.center,
+                TextSection(
+                  title: 'Additional informations',
+                  content:
+                      'The user name couldn\'t contain spaces or special characters. The password should be longer than 5 characters.',
+                  maxHeight: media.size.height * 0.2,
                 ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(top: 40),
-                child: Text(
-                  'Additional informations',
-                  style: Theme.of(context).textTheme.headline1,
-                  textAlign: TextAlign.center,
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(top: 10),
-                child: Text(
-                  'The user name couldn\'t contain spaces or special characters. The password should be longer than 5 characters.',
-                  style: Theme.of(context).textTheme.headline2,
-                  textAlign: TextAlign.center,
-                ),
-              ),
-            ],
+                Center(
+                  child: TextButton(
+                    onPressed: () => Navigator.pop(context),
+                    child: Text(
+                      'Understood',
+                    ),
+                  ),
+                )
+              ],
+            ),
           ),
         ),
       ),
