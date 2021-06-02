5fcb7e9a6dcc0f69fd005846e6b96a16d258d23e
Gabriele <gabri.cabal@gmail.com>
2021-06-02 19:09:03 +0200

    Improvement

--- my_older/lib/pages/login-info-page.dart
+++ my_older/lib/pages/login-info-page.dart
@@ -51,7 +51,7 @@ class LoginInfoPage extends StatelessWidget {
                       'Understood',
                     ),
                   ),
-                )
+                ),
               ],
             ),
           ),
