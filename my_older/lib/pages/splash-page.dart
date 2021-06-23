df441506c0b376cda69097ec1286e8f434894084
Gabriele <gabri.cabal@gmail.com>
2021-06-23 18:25:41 +0200

    Increased quality

--- my_older/lib/pages/splash-page.dart
+++ my_older/lib/pages/splash-page.dart
@@ -14,7 +14,6 @@ class SplashPage extends StatefulWidget {
   @override
   State<StatefulWidget> createState() => _SplashPageState();
 }
-
 class _SplashPageState extends State<SplashPage> {
   @override
   Widget build(BuildContext context) {
