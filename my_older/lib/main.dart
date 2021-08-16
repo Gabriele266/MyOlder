f532b479e15f75e3972c10128227d79fd4526631
Gabriele Cavallo <gabri.cabal@gmail.com>
2021-08-16 22:55:18 +0200

    Removed unuseful instructions

--- my_older/lib/main.dart
+++ my_older/lib/main.dart
@@ -1,6 +1,13 @@
 import 'package:flutter/material.dart';
 
 import 'myolder-app.dart';
+import './providers/settings/settings-manager.dart';
 
 // Execute the application
-void main() => runApp(MyOlderApp());
+void main() {
+  // Settings firstSettings = Settings();
+  // firstSettings.keepLogin.value = true;
+  //
+  // firstSettings.saveSettings();
+  runApp(MyOlderApp());
+}
